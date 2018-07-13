03290564afbd788187f398d255a1a8e827a1be17
mmedhat94 <mostafa.medhat994@gmail.com>
2018-07-13 17:38:21 +0200

    Update node&&npm versions to fix heroku boot timeout bug 3rd trial

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const port = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+// const port = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(port, {useMongoClient: true});
+mongoose.connect((process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'), {useMongoClient: true});
 
 module.exports.mongoose = mongoose;
