dd0b21e8c8f00ff4716b287fec25814e51451e9f
mmedhat94 <mostafa.medhat994@gmail.com>
2018-07-13 16:35:37 +0200

    Fix heroku boot timeout bug 2nd trial

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
+const port = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+mongoose.connect(port, {useMongoClient: true});
 
 module.exports.mongoose = mongoose;
