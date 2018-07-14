be2b0ea2716cab8ea7e8aa5025047a5faed2e125
mmedhat94 <mostafa.medhat994@gmail.com>
2018-07-14 14:43:11 +0200

    Refactor the app rotues using app.route()

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const router = express.Router();
 
 // Local Imports
 const {mongoose} = require('./db/mongoose');
@@ -11,79 +12,157 @@ const {User} = require('./models/user');
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+// <<<<<<< MIDDLEWARE HANDLERS >>>>>>>
 app.use(bodyParser.json());
+// <<<<<<<<<<<<<<<<->>>>>>>>>>>>>>>>>>>
 
 
-app.post('/todos', (req, res, next) => {
-    const todo = new Todo({
-        text: req.body.text
-    });
+// <<<<<<< ROUTES >>>>>>>
+app.route('/todos')
 
-    todo.save()
-        .then((doc) => {
-            res.send(doc);
-        })
-        .catch((error) => {
-            res.status(400).send(error);
-        })
-});
-
-app.get('/todos', (req, res, next) => {
-    Todo.find()
+    // Get the data from (GET localhost:3000/todos)
+   .get((req, res, next) => {
+        Todo.find()
         .then((todos) => {
             res.send({ todos });
         })
         .catch((error) => {
             res.status(400).send(error);
         });
-});
+   })
 
-app.get('/todos/:id', (req, res, next) => {
-    const id = req.params.id;
-    
-    // Validate user id using isValid()
-    if (!ObjectID.isValid(id)) {
+   // Post the data to (POST localhost:3000/todos)
+   .post((req, res, next) => {
+        const todo = new Todo({
+            text: req.body.text
+        });
         
-        res.status(404).send({})
-    }
+        todo.save()
+            .then((doc) => {
+                res.send(doc);
+            })
+            .catch((error) => {
+                res.status(400).send(error);
+            })
+   })
 
-    // Find todo by ID
-    Todo.findById(id)
-        .then((todo) => {
-            if (!todo) {
-                return res.status(404).send();
-            }
-            res.send({todo});
-        })
-        .catch((error) => {
-            res.status(404).send(error);
-        });
-});
 
-app.delete('/todos/:id', (req, res, next) => {
-    const id = req.params.id;
+app.route('/todos/:id')
 
-    // Validate user id using isValid()
-    if (!ObjectID.isValid(id)) {
-        res.status(404).send({})
-    }
+   // Get a specific todo by ID (GET localhost:3000/todos/:id)
+   .get((req, res, next) => {
+        const id = req.params.id;
+        
+        // Validate user id using isValid()
+        if (!ObjectID.isValid(id)) {
+            
+            res.status(404).send({})
+        }
+        
+        // Find todo by ID
+        Todo.findById(id)
+            .then((todo) => {
+                if (!todo) {
+                    return res.status(404).send();
+                }
+                res.send({todo});
+            })
+            .catch((error) => {
+                res.status(404).send(error);
+            });
+   })
 
-    Todo.findByIdAndRemove(id)
-        .then((todo) => {
-            if (!todo) {
-                return res.status(404).send();
-            }
-            res.send({todo});
-        })
-        .catch((error) => {
-            res.status(404).send(error);
-        });
-});
+   // Delete a specific todo by ID (DELETE localhost:3000/todos/:id)
+   .delete((req, res, next) => {
+        const id = req.params.id;
+        
+        // Validate user id using isValid()
+        if (!ObjectID.isValid(id)) {
+            res.status(404).send({})
+        }
+
+        // Find todo by ID
+        Todo.findByIdAndRemove(id)
+            .then((todo) => {
+                if (!todo) {
+                    return res.status(404).send();
+                }
+                res.send({todo});
+            })
+            .catch((error) => {
+                res.status(404).send(error);
+            });
+   })
 
 
 app.listen(port, () => {
     console.log(`Startrd on port ${port}`);
-})
+});
 
 module.exports.app = app;
+
+// app.post('/todos', (req, res, next) => {
+//     const todo = new Todo({
+//         text: req.body.text
+//     });
+
+//     todo.save()
+//         .then((doc) => {
+//             res.send(doc);
+//         })
+//         .catch((error) => {
+//             res.status(400).send(error);
+//         })
+// });
+
+// app.get('/todos', (req, res, next) => {
+//     Todo.find()
+//         .then((todos) => {
+//             res.send({ todos });
+//         })
+//         .catch((error) => {
+//             res.status(400).send(error);
+//         });
+// });
+
+// app.get('/todos/:id', (req, res, next) => {
+//     const id = req.params.id;
+    
+//     // Validate user id using isValid()
+//     if (!ObjectID.isValid(id)) {
+        
+//         res.status(404).send({})
+//     }
+
+//     // Find todo by ID
+//     Todo.findById(id)
+//         .then((todo) => {
+//             if (!todo) {
+//                 return res.status(404).send();
+//             }
+//             res.send({todo});
+//         })
+//         .catch((error) => {
+//             res.status(404).send(error);
+//         });
+// });
+
+// app.delete('/todos/:id', (req, res, next) => {
+//     const id = req.params.id;
+
+//     // Validate user id using isValid()
+//     if (!ObjectID.isValid(id)) {
+//         res.status(404).send({})
+//     }
+
+//     Todo.findByIdAndRemove(id)
+//         .then((todo) => {
+//             if (!todo) {
+//                 return res.status(404).send();
+//             }
+//             res.send({todo});
+//         })
+//         .catch((error) => {
+//             res.status(404).send(error);
+//         });
+// });
